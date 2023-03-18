27a079e41642f1b25d086397e6e7cbe895034fe9
phu14ss0 <wodyd0312@sch.ac.kr>
2023-03-18 17:50:54 +0900

    upload taint.py

--- taint.py
+++ taint.py
@@ -1,15 +1,14 @@
 class NewCommand(GenericCommand):
     """Dummy new command."""
     _cmdline_ = "newcmd"
-    _syntax_  = "{:s}".format(_cmdline_)
+    _syntax_  = f"{_cmdline_}"
 
     @only_if_gdb_running         # not required, ensures that the debug session is started
     def do_invoke(self, argv):
-        # do anything allowed by gef, for example show the current running
-        # architecture as Python object:
-        print(" = {}".format(current_arch) )
+        # let's say we want to print some info about the architecture of the current binary
+        print(f"gef.arch={gef.arch}")
         # or showing the current $pc
-        print("pc = {:#x}".format(current_arch.pc))
+        print(f"gef.arch.pc={gef.arch.pc:#x}")
         return
 
 register_external_command(NewCommand())
